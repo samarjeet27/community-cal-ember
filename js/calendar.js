bffa55af9ddeab87b66330de245026ae8f243610
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-15 21:16:27 +0530

    Wrap as Ember Component

--- js/calendar.js
+++ js/calendar.js
@@ -1,12 +1,67 @@
 // create ember-app instance
-var BLUGCalendarApp = Ember.Application.create({
-});
-
+var BLUGCalendarApp = Ember.Application.create();
 // events
-var _events;
+var _events = [];
+
+// Formatting helpers
+(function () {
+    var helpers = [{
+        name: 'date',
+        date: function(unix) {
+            var date = new Date(unix * 1000);
+            return date.toDateString();
+        }
+    }, {
+        name: 'day',
+        day: function(unix) {
+            var date = new Date(unix * 1000);
+            return date.getDate();
+        },
+    }, {
+        name: 'month',
+        month: function(unix) {
+            var date = new Date(unix * 1000);
+            return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][date.getMonth()];
+        },
+    }, {
+        name: 'year',
+        year: function(unix) {
+            var date = new Date(unix * 1000);
+            return date.getFullYear();
+        },
+    }, {
+        name: 'time',
+        time: function(unix) {
+            var date = new Date(unix * 1000);
+            return date.getTime();
+        }
+    }, {
+        name: 'log',
+        log: function(x) {
+            console.log(x);
+        }
+    }];
+
+
+    for (var i = 0; i < helpers.length; i++) {
+        var helper = helpers[i];
+        Ember.Handlebars.helper(helper.name, helper[helper.name]);
+    }
+
+    // used handlebars as it works differently
+    Handlebars.registerHelper('link', function(id) {
+        id = Handlebars.Utils.escapeExpression(id);
+        template = '#/communitycal/';
+        // don't need the ending tag (safe escape)
+        var result = '<a href="'+ template + id + '">';
+
+        return new Handlebars.SafeString(result);
+    });
+
+}) ();
 
 // initialise tabs
-BLUGCalendarApp.ApplicationView = Ember.View.extend({
+BLUGCalendarApp.CommunityCalendarComponent = Ember.Component.extend({
     didInsertElement : function(){
         Ember.run.next(function(){
             $('#tab-container').easytabs({
@@ -23,6 +78,10 @@ var steam = Steam.create(BLUGCalendarApp);
 	}
 );
 
-BLUGCalendarApp.ApplicationController = Ember.Controller.extend({
-	events: _events
+BLUGCalendarApp.ApplicationRoute = Ember.Route.extend({
+    model: function() {
+        return {
+            cevents: _events
+        }
+    }
 });
